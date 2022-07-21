4e6b7c1f4da700a7f8fc8d31522c9899fa886f57
ThiagoLima <thiiago.limaa@gmail.com>
2022-07-21 16:29:00 -0300

    atualização projetos

---
+++ curso-javascript/Modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade('Thiago Lima', 26));
+console.log(mostraCidade('Thiago Lima', 'Brasília'));
+console.log(mostraHobby('Thiago Lima', 'Futebol'));
